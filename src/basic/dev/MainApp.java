7652aa289a43c9be41d808ca021fdb574d4d924e
minh <trananhminhcv@gmail.com>
2021-10-25 14:35:59 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+import java.security.PublicKey;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//Comment 1 dòng cách 1. Phím nóng Ctr+ /
+		/*Comment 1 dòng cách 2*/
+		System.out.println("Hello World");
+		int number;
+		String nameSchool ;
+	}
+	/*
+	 *  tênphươngthức tuântheoquytắc camelCase
+	 */
+	public void show () {
+	}
+}
